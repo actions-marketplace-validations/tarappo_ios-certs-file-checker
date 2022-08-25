9c86f61876f25446c04460d4fdfcdcd6ac64c201
tarappo <tarappo@gmail.com>
2022-08-25 14:25:06 +0900

    add checks ios certs file scripts

---
+++ Gemfile
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+gem 'fastlane'
+gem 'apple_certs_info'
+gem 'slack-notifier'
---
+++ scripts/check_certs_profile.rb
@@ -0,0 +1,71 @@
+require 'apple_certs_info'
+require 'slack-notifier'
+
+class Message
+  # expireが近いものから必要な情報を生成
+  def self.expire_list(list:, type:)
+    args = :cname
+    case type
+    when :distribution_certificate, :development_certificate then
+        args = :cname
+    when :provisioning_profile then
+        args = :app_id_name
+    end
+    
+    message = []
+    list.each do |data|
+      message << "#{data[args]}（残り約#{data[:limit_days]}日）"
+    end
+
+    message
+  end
+
+   # slackに投稿するためのメッセージの整形
+   def self.create_slack_message(message_list:, type:)
+    case type
+    when :distribution_certificate then
+        prefix_message = "[Certificate(Distribution)]\n"
+    when :development_certificate then
+        prefix_message = "[Certificate(Development)]\n"
+    when :provisioning_profile then
+        prefix_message = "[Provisioning Profile]\n"
+    end
+    
+    message = prefix_message + message_list.join("\n") + "\n\n"
+
+    message
+  end
+end
+
+
+limit_days = ENV["EXPIRE_LIMIT_DAYS"]
+slack_webhook_url = ENV["SLACK_WEBHOOK_URL"]
+extra_message = ENV["EXTRA_MESSAGE"]
+
+# provisioning profile
+expire_pp_list = AppleCertsInfo.provisioning_profile_list_limit_days_for(days: limit_days)
+expire_pp_message = Message.expire_list(list: expire_pp_list, type: :provisioning_profile)
+puts expire_pp_message
+
+# certificate
+expire_dev_cert_list = AppleCertsInfo.certificate_development_list_limit_days_for(days: limit_days)
+expire_dev_cert_message = Message.expire_list(list: expire_dev_cert_list, type: :development_certificate)
+puts expire_dev_cert_message
+
+expire_dist_cert_list = AppleCertsInfo.certificate_distribution_list_limit_days_for(days: limit_days)
+expire_dist_cert_message = Message.expire_list(list: expire_dist_cert_list, type: :development_certificate)
+puts expire_dist_cert_message
+
+
+# SLACK通知先がある場合
+unless slack_webhook_url.nil?
+    expire_pp = Message.create_slack_message(message_list: expire_pp_message, type: :provisioning_profile)
+    expire_dev_cert = Message.create_slack_message(message_list: expire_dev_cert_message, type: :development_certificate)
+    expire_dist_cert = Message.create_slack_message(message_list: expire_dist_cert_message, type: :distribution_certificate)
+
+    slack_message = expire_pp + expire_dev_cert + expire_dist_cert
+    slack_message +=  extra_message unless extra_message.nil?
+
+   notifier = Slack::Notifier.new slack_webhook_url
+   notifier.ping slack_message
+end
